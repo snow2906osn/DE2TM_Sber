49766a0197de5c1a8b4ff75f0dbc2c44a62fe30b
snow2906osn <24krsk@protonmail.com>
2022-04-02 09:47:22 +0700

    Групповой проект модули 5 и 2

---
+++ Групповой проект/Блок - 2/Кредитные заявки - без CTE.sql
@@ -0,0 +1,35 @@
+---Блок 2
+/* Тестовые данне
+select count(distinct client_id) from de_common.group_dim_client; -- 136 Клиентов
+select count(distinct client_id) from de_common.group_fct_credit_applications where to_date('2021-12-01','YYYY-MM-DD')>=application_date; -- 133 Клиента, 3 отсутсвуют!
+select count(*) from de_common.group_fct_credit_applications where to_date('2021-12-01','YYYY-MM-DD')>=application_date; -- 191 строка в периоде на дату отчета
+ */
+
+-- 3 Клиента (46812,113122,147246) не имеют заявок в отчетном периоде, в поле LAST_APP_MONTH_CNT указано техническое значение -1
+
+ALTER SESSION SET nls_date_language = 'AMERICAN';
+ALTER SESSION SET  NLS_DATE_FORMAT='DD-MM-YYYY';
+
+select dc.client_id,
+       count(cte.application_id) APP_HIST_CNT,
+       count(case when cte.diff_m<=6 then cte.application_id else null end) APP_6M_CNT,
+       count(case when cte.diff_m<=3 then cte.application_id else null end) APP_3M_CNT,
+       sum(nvl(cte.application_sum_amt,0)) APP_HIST_AMT,
+       sum(case when cte.diff_m<=6 then cte.application_sum_amt else 0 end) APP_6M_AMT,
+       sum(case when cte.diff_m<=3 then cte.application_sum_amt else 0 end) APP_3M_AMT,
+       max(nvl(cte.i_flg,0)) MORTGAGE_6M_FLG,
+       min(nvl(cte.diff_m,-1)) LAST_APP_MONTH_CNT
+from de_common.group_dim_client dc
+     left join (select
+                  floor(months_between(to_date('2021-12-01','YYYY-MM-DD'), ca.application_date)) diff_m,
+                  ca.application_id,
+                  ca.client_id,
+                  ca.application_date,
+                  ca.application_sum_amt,
+                  cp.product_nm,
+                  case when floor(months_between(to_date('2021-12-01','YYYY-MM-DD'), ca.application_date))<=6 and cp.product_nm='Ипотека' then 1 else 0 end i_flg
+                from de_common.group_fct_credit_applications ca
+                  inner join de_common.group_dict_credit_product cp on ca.credit_product_type=cp.poduct_type
+                  and to_date('2021-12-01','YYYY-MM-DD')>=ca.application_date) cte /* на 01.12.2021 имеется запись */ on dc.client_id=cte.client_id
+group by dc.client_id
+order by dc.client_id
---
+++ Групповой проект/Блок - 5/Зарплатные транзакции - форматирование сумм без CTE.sql
@@ -0,0 +1,61 @@
+-- √рупповой проект - блок 5 "«арплатные транзакции"
+/* “естова€ выборка
+select * from de_common.group_dict_income_type;
+
+--2021-01-01, 2021-12-01 и 2094
+select min(to_date(income_month,'YYYY-MM')) dmin, max(to_date(income_month,'YYYY-MM')) dmax, count(client_id) from de_common.group_fct_income_transactions;
+
+ */
+
+ALTER SESSION SET nls_date_language = 'AMERICAN';
+
+select
+       -- —умма транзакций за первый мес€ц до отчетной даты
+       to_char(sum(case when cte.diff_m=1 then cte.income_sum_amt else 0 end),'99G999G990D99') SALARY_2M_AMT, /* !!! наименование на основании “«, но по идее SALARY_1M_AMT*/
+       -- —умма транзакций за второй мес€ц до отчетной даты
+       to_char(sum(case when cte.diff_m=2 then cte.income_sum_amt else 0 end),'99G999G990D99') SALARY_1M_AMT, /* !!! наименование на основании “«, но по идее SALARY_2M_AMT*/
+       -- —умма транзакций за третий мес€ц до отчетной даты
+       to_char(sum(case when cte.diff_m=3 then cte.income_sum_amt else 0 end),'99G999G990D99') SALARY_3M_AMT,
+       -- —умма транзакций за четвертый мес€ц до отчетной даты
+       to_char(sum(case when cte.diff_m=4 then cte.income_sum_amt else 0 end),'99G999G990D99') SALARY_4M_AMT,
+       -- —умма транзакций за п€тый мес€ц до отчетной даты
+       to_char(sum(case when cte.diff_m=5 then cte.income_sum_amt else 0 end),'99G999G990D99') SALARY_5M_AMT,
+       -- —умма транзакций за шестой мес€ц до отчетной даты
+       to_char(sum(case when cte.diff_m=6 then cte.income_sum_amt else 0 end),'99G999G990D99') SALARY_6M_AMT,
+       --  ол-во транзакций за первый мес€ц до отчетной даты
+       count(case when cte.diff_m=1 then cte.income_sum_amt else null end) SALARY_1M_CNT,
+       --  ол-во транзакций за второй мес€ц до отчетной даты
+       count(case when cte.diff_m=2 then cte.income_sum_amt else null end) SALARY_2M_CNT,
+       --  ол-во транзакций за третий мес€ц до отчетной даты
+       count(case when cte.diff_m=3 then cte.income_sum_amt else null end) SALARY_3M_CNT,
+       --  ол-во транзакций за четвертый мес€ц до отчетной даты
+       count(case when cte.diff_m=4 then cte.income_sum_amt else null end) SALARY_4M_CNT,
+       --  ол-во транзакций за п€тый мес€ц до отчетной даты
+       count(case when cte.diff_m=5 then cte.income_sum_amt else null end) SALARY_5M_CNT,
+       --  ол-во транзакций за шестой мес€ц до отчетной даты
+       count(case when cte.diff_m=6 then cte.income_sum_amt else null end) SALARY_6M_CNT,
+       -- ќтношение суммы зарплатных транзакций за последний мес€ц к сумме за 3 последних мес€ца
+       round(sum(case when cte.diff_m=1 then cte.income_sum_amt else 0 end)/sum(case when cte.diff_m<=3 then cte.income_sum_amt else 0 end),2) SALARY_1M_TO_3M_AMT_PCT,
+       -- ќтношение суммы зарплатных транзакций за последний мес€ц к сумме за 6 последних мес€цев
+       round(sum(case when cte.diff_m=1 then cte.income_sum_amt else 0 end)/sum(case when cte.diff_m<=6 then cte.income_sum_amt else 0 end),2) SALARY_1M_TO_6M_AMT_PCT,
+       -- ќтношение кол-ва зарплатных транзакций за последний мес€ц к кол-ву за 3 последних мес€ца
+       round(count(case when cte.diff_m=1 then cte.income_sum_amt else null end)/count(case when cte.diff_m<=3 then cte.income_sum_amt else null end),2) SALARY_1M_TO_3M_CNT_PCT,
+       -- ќтношение кол-ва зарплатных за последний мес€ц к кол-ву за 6 последних мес€цев
+       round(count(case when cte.diff_m=1 then cte.income_sum_amt else null end)/count(case when cte.diff_m<=6 then cte.income_sum_amt else null end),2) SALARY_1M_TO_6M_CNT_PCT,
+       --  ол-во мес€цев, в которые были зарплатные начислени€, из последних 6 мес€цев
+       count(distinct case when cte.diff_m<=6 then cte.diff_m else null end) SALARY_DURING_6M_CNT,
+       --  ол-во мес€цев назад, когда была начислена последн€€ зарплатна€ транзакци€
+       min(diff_m) LAST_SAL_TRANS_MONTH_CNT,
+       --  ол-во мес€цев назад, когда была начислена перва€ зарплатна€ транзакци€
+       max(diff_m) FIRST_SAL_TRANS_MONTH_CNT
+from (select it.client_id,
+      months_between(to_date('2021-12-01','YYYY-MM-DD'), to_date(it.income_month,'YYYY-MM')) diff_m,
+      it.income_sum_amt,
+      it.transaction_id
+      from
+           de_common.group_fct_income_transactions it
+           inner join de_common.group_dict_income_type di on it.income_type=di.income_type
+           and (di.income_nm='«арплатное начисление' and
+                to_date('2021-12-01','YYYY-MM-DD')>to_date(it.income_month,'YYYY-MM'))) cte
+group by cte.client_id
+order by cte.client_id;
